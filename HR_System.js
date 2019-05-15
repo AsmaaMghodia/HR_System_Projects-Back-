ec72acb0326d80297d307a3597645be266ce588c
asmaa ghodia <you@example.com>
2019-05-15 04:55:43 +0200

    test projects task

---
+++ HR_System.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const Joi = require('joi');
+const projects = require('./models/project');
+app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/HR-System', { useNewUrlParser: true })
+    .then(() => console.log('connected to db successfully'))
+    .catch((err) => console.log(err));
+
+
+projects.insertMany([{
+    name: 'Nooon',
+    projectRate: "good",
+    notes: "online shooping",
+    employees: ['ahmed', 'soha'],
+
+}, ])
+
+app.get('/HR-System', async(req, res) => {
+    console.log("get");
+    const result = await projects.find();
+    res.send(result);
+})
+
+
+
+app.post('/HR-System', async(req, res) => {
+    console.log("post")
+    const result = await projects.create(req.body);
+    console.log("post");
+    res.send("added successfully ");
+})
+
+
+app.delete('/HR-System/:name', async(req, res) => {
+    try {
+        const result = await projects.find({ name: req.params.name });
+        if (result.length > 0) {
+            const deletedEmployee = await projects.deleteMany({ name: result[0].name })
+            res.send(`${deletedEmployee.deletedCount} projects have the same name have been deleted`);
+
+        } else {
+            res.send('This project is not exist');
+
+        }
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+app.put('/HR-System/:name', async(req, res) => {
+    try {
+        console.log("put")
+        const fresult = await projects.find({ name: req.params.name });
+        if (fresult.length > 0) {
+            const result = await projects.findOneAndUpdate(req.params.name, {
+                name: req.body.name,
+                projectRate: req.body.projectRate,
+                notes: req.body.notes,
+                employees: req.body.employees
+            }, {
+                new: true
+            });
+            res.send("changed data successfully");
+
+        } else {
+            res.send('This project is not exist');
+
+        }
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+app.listen(4000, () => console.log('listining for requests...'))
---
+++ models/project.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const projectsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    projectRate: {
+        type: String,
+        required: true
+    },
+    notes: {
+        type: String,
+        required: true
+    },
+    employees: {
+        type: [String],
+        required: true
+    }
+});
+const projects = mongoose.model("projects", projectsSchema);
+// note that we use a pascal naming convention for model
+module.exports = projects;
